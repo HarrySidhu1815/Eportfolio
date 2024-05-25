9562a9494f8897f40df29f0f93fc64a347ffefff
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-05-25 08:00:54 -0700

    Corousel updated on project section

--- portfolio/src/components/WorkCard/WorkCard.js
+++ portfolio/src/components/WorkCard/WorkCard.js
@@ -1,48 +1,50 @@
-import React, { useState } from 'react'
+import React from 'react'
 import UniFlip from '../../assests/Uniflip.jpeg'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import SidhuMarket from '../../assests/sidhu.jpeg'
 import Project from './Project'
 import './workCard.css'
 
-const projectData = [
-    { source: UniFlip, title: 'UniFlip', description: 'UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.' },
-    { source: UniFlip, title: 'UniFlip', description: 'UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.' },
-    { source: SidhuMarket, title: 'SidhuMarket', description: 'UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.' },
-    { source: SidhuMarket, title: 'SidhuMarket', description: 'UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.' },
-    { source: UniFlip, title: 'UniFlip', description: 'UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.' },
-];
+// const projectData = [
+//     { source: UniFlip, title: 'UniFlip', description: 'UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.' },
+//     { source: UniFlip, title: 'UniFlip', description: 'UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.' },
+//     { source: SidhuMarket, title: 'SidhuMarket', description: 'UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.' },
+//     { source: SidhuMarket, title: 'SidhuMarket', description: 'UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.' },
+//     { source: UniFlip, title: 'UniFlip', description: 'UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.' },
+// ];
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 3
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 2
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
 const WorkCard = ({goBack}) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    const handleNext = () => {
-        setCurrentIndex((currentIndex + 1) % projectData.length);
-    };
-
-    const handlePrevious = () => {
-        setCurrentIndex((currentIndex - 1 + projectData.length) % projectData.length);
-    };
     return (
         <>
             <h1 className='project-heading'>My Projects</h1>
-            <div className="projects-carousel">
-                {currentIndex > 0 && <button className="carousel-btn prev-btn" onClick={handlePrevious}>Back</button>}
                 <div className="project-container">
-                    {projectData.slice(currentIndex, currentIndex + 2).map((project, index) => (
-                        <Project key={index} source={project.source} title={project.title}>
-                            {project.description}
-                        </Project>
-                    ))}
-                    {/* Handling case when slicing over the array length */}
-                    {currentIndex + 2 > projectData.length && projectData.slice(0, (currentIndex + 2) % projectData.length).map((project, index) => (
-                        <Project key={projectData.length + index} source={project.source} title={project.title}>
-                            {project.description}
-                        </Project>
-                    ))}
+                    <Carousel responsive={responsive} infinite={true} className='carousel'> 
+                        <Project source={SidhuMarket} title={'Sidhu Market'}>UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.</Project>
+                        <Project source={UniFlip} title={'Sidhu Market'}>UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.</Project>
+                        <Project source={SidhuMarket} title={'Sidhu Market'}>UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.</Project>
+                        <Project source={UniFlip} title={'Sidhu Market'}>UniFlip is a React-based marketplace app for selling used school supplies, featuring real-time chat with Node.js and Socket.io. As group leader, I utilized software engineering principles, including data modeling and design patterns, to develop this project.</Project>
+                    </Carousel>
                 </div>
-                {currentIndex < projectData.length - 2 && <button className="carousel-btn next-btn" onClick={handleNext}>Next</button>}
-            </div>
-            {/* <button className="worksBtn" onClick={goBack()}>See Portfolio</button> */}
         </>
     )
 }
