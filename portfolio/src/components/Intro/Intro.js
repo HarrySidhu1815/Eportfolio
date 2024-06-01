eab1315b86f6d2ab92e457e119263568c5b6df9c
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-06-01 08:55:36 -0700

    missionstatement added

--- portfolio/src/components/Intro/Intro.js
+++ portfolio/src/components/Intro/Intro.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './intro.css'
 import {Link} from 'react-scroll'
 import bg from '../../assests/profile.png'
-import btn from '../../assests/hire.png'
+// import btn from '../../assests/hire.png'
 
 
 const Intro = () => {
+  const [showMission, setShowMission] = useState(false)
   return (
-      <section id="intro">
-        <div className="introContent">
+    <section id="intro">
+      {!showMission ? (<><div className="introContent">
             <span className="hello">Hello</span>
             <span className="introText">I'm <span className="introName">Harjobanpreet</span><br/>Web Developer</span>
             <p className="introPara">I am a skilled web developer with experience in creating <br/> full stack website usign MERN languages</p>
-            <Link to='contact' spy={true} smooth={true} offset={-100} duration={500}><button className="btn"><img src={btn} alt="Hire Me" className='btnImg'/>Hire Me</button></Link>
+            {/* <Link to='contact' spy={true} smooth={true} offset={-100} duration={500}><button className="btn">View Mission</button></Link> */}
+            <Link><button className='btn' onClick={() => {
+              setShowMission(true)
+            }}>View Mission</button></Link>
         </div>
-        <img src={bg} alt="Profile" className="bg" />
+        <img src={bg} alt="Profile" className="bg" /></>) : <div className="missionContent">
+            <span className="missionTitle">MISSION</span>
+            <span className="mission">Empowering businesses by providing innovative digital solutions, ensuring accessibility and efficiency for everyone, so they can thrive in the digital age.</span>
+            <p className="rationale">My mission statement focuses on fostering innovation and sustainable development, closely aligning with the United Nations Sustainable Development Goal 9 (Industry, Innovation, and Infrastructure). By providing cutting-edge web development solutions, I aim to enhance businesses' digital presence, making technology accessible and efficient for all. This mission supports sustainable industrialization and promotes innovation, helping businesses to adapt, grow, and succeed in the rapidly evolving digital landscape. In doing so, I contribute to building resilient infrastructure and fostering innovation, which are essential for long-term economic growth and societal advancement.</p>
+            <Link><button className='btn' onClick={() => {
+              setShowMission(false)
+            }}>Hide Mission</button></Link>
+        </div>}
       </section>
+      
   )
 }
 
--- portfolio/src/components/Works/Works.js
+++ portfolio/src/components/Works/Works.js
@@ -13,12 +13,40 @@ const Works = () => {
       <h2 className="worksTitle">My Portfolio</h2>
       <span className="worksDesc">I take pride in paying attention to the smallest details and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.</span>
       <div className="worksImgs">
-        <img src={Port1} alt="" className="worksImg" />
+        <div className='imgwrapper'>
+          <img src={Port1} alt="" className="worksImg" />
+          <div className='softskillDesc'>
+            <h2 className='softskillTitle'>Punctuality</h2>
+            Demonstrated punctuality by consistently arriving on time for shifts and meeting all deadlines, contributing to efficient store operations.</div>
+        </div>  
+        <div className='imgwrapper'>
         <img src={Port2} alt="" className="worksImg" />
+          <div className='softskillDesc'>
+            <h2 className='softskillTitle'>Decision Making</h2>
+            Demonstrated ability to make informed and effective decisions to optimize product placement and enhance sales performance.        </div>  
+        </div>
+        <div className='imgwrapper'>
         <img src={Port3} alt="" className="worksImg" />
+          <div className='softskillDesc'>
+            <h2 className='softskillTitle'>Teamwork</h2>
+            Collaborated effectively with cross-functional teams to achieve store goals and enhance overall performance.
+                    </div>  
+        <div className='imgwrapper'>
         <img src={Port4} alt="" className="worksImg" />
+          <div className='softskillDesc'>
+            <h2 className='softskillTitle'>Communication</h2>
+            Excellent verbal and written communication skills, effectively interacting with customers, colleagues, and management to enhance collaboration and customer satisfaction.        
+          </div>  
+        <div className='imgwrapper'>
         <img src={Port5} alt="" className="worksImg" />
+          <div className='softskillDesc'>
+            <h2 className='softskillTitle'>Problem Solving</h2>
+            Effectively resolve inventory discrepancies and customer issues with proactive and creative solutions.        </div>  
+        <div className='imgwrapper'>
         <img src={Port6} alt="" className="worksImg" />
+          <div className='softskillDesc'>
+            <h2 className='softskillTitle'>Flexible</h2>
+            Adaptable and flexible, able to efficiently handle changing priorities and diverse tasks in a fast-paced retail environment.        </div>     
       </div>
     </section>
   )
