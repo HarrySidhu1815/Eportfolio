662cc80674194e08975cacd20d28eccd6039a914
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-06-02 08:02:27 -0700

    portfolio section updated

--- portfolio/src/components/Works/Works.js
+++ portfolio/src/components/Works/Works.js
@@ -18,35 +18,39 @@ const Works = () => {
           <div className='softskillDesc'>
             <h2 className='softskillTitle'>Punctuality</h2>
             Demonstrated punctuality by consistently arriving on time for shifts and meeting all deadlines, contributing to efficient store operations.</div>
-        </div>  
-        <div className='imgwrapper'>
-        <img src={Port2} alt="" className="worksImg" />
-          <div className='softskillDesc'>
-            <h2 className='softskillTitle'>Decision Making</h2>
-            Demonstrated ability to make informed and effective decisions to optimize product placement and enhance sales performance.        </div>  
         </div>
         <div className='imgwrapper'>
-        <img src={Port3} alt="" className="worksImg" />
+          <img src={Port2} alt="" className="worksImg" />
+          <div className='softskillDesc'>
+            <h2 className='softskillTitle'>Decision Making</h2>
+            Demonstrated ability to make informed and effective decisions to optimize product placement and enhance sales performance.        </div>
+        </div>
+        <div className='imgwrapper'>
+          <img src={Port3} alt="" className="worksImg" />
           <div className='softskillDesc'>
             <h2 className='softskillTitle'>Teamwork</h2>
             Collaborated effectively with cross-functional teams to achieve store goals and enhance overall performance.
-                    </div>  
+          </div>
+        </div>
         <div className='imgwrapper'>
-        <img src={Port4} alt="" className="worksImg" />
+          <img src={Port4} alt="" className="worksImg" />
           <div className='softskillDesc'>
             <h2 className='softskillTitle'>Communication</h2>
-            Excellent verbal and written communication skills, effectively interacting with customers, colleagues, and management to enhance collaboration and customer satisfaction.        
-          </div>  
+            Excellent verbal and written communication skills, effectively interacting with customers, colleagues, and management to enhance collaboration and customer satisfaction.
+          </div>
+        </div>
         <div className='imgwrapper'>
-        <img src={Port5} alt="" className="worksImg" />
+          <img src={Port5} alt="" className="worksImg" />
           <div className='softskillDesc'>
             <h2 className='softskillTitle'>Problem Solving</h2>
-            Effectively resolve inventory discrepancies and customer issues with proactive and creative solutions.        </div>  
+            Effectively resolve inventory discrepancies and customer issues with proactive and creative solutions.        </div>
+        </div>
         <div className='imgwrapper'>
-        <img src={Port6} alt="" className="worksImg" />
+          <img src={Port6} alt="" className="worksImg" />
           <div className='softskillDesc'>
             <h2 className='softskillTitle'>Flexible</h2>
-            Adaptable and flexible, able to efficiently handle changing priorities and diverse tasks in a fast-paced retail environment.        </div>     
+            Adaptable and flexible, able to efficiently handle changing priorities and diverse tasks in a fast-paced retail environment.        </div>
+        </div>
       </div>
     </section>
   )
